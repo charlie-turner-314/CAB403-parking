29bfabba423d714ae762b4421a587c7a6a47a320
Kiara Maier <kiaramaier04@gmail.com>
2022-10-10 22:15:15 +1000

    added boom and sign status to display

--- src/display.c
+++ src/display.c
@@ -22,14 +22,29 @@ void *display_handler(void *arg) {
         // print each entrance LPR for each level
         for (int i = 0; i < NUM_ENTRANCES; i++) {
             pthread_mutex_lock(&shm->entrances[i].lpr.mutex);
-            char plate[6];
-            memcpy(plate, shm->entrances[i].lpr.plate, 6);
+            char plate[7];
+            memcpy(plate, shm->entrances[i].lpr.plate, 7);
+            char boom = shm->entrances[i].gate.status;
+            char sign = shm->entrances[i].sign.display;
             pthread_mutex_unlock(&shm->entrances[i].lpr.mutex);
-            printf("Entry LPR %d: %6s \n", i, plate);
-
+            printf("Entry LPR %d: %6s | ", i, plate);
+            printf("'%c' | ", boom  == 0 ? ' ' : boom);
+            printf("'%c' |\n", sign  == 0 ? ' ' : sign);
             // wait 50ms
             usleep(50000);
         }
+
+        //   // print each exit LPR for each level
+        // for (int i = 0; i < NUM_EXITS; i++) {
+        //     pthread_mutex_lock(&shm->exits[i].lpr.mutex);
+        //     char plate[7];
+        //     memcpy(plate, shm->exits[i].lpr.plate, 7);
+        //     pthread_mutex_unlock(&shm->exits[i].lpr.mutex);
+        //     printf("Exit LPR %d: %6s \n", i, plate);
+
+        //     // wait 50ms
+        //     usleep(50000);
+        // }
     }
     return NULL;
 }

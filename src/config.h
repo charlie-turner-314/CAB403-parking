c00a55b5bde1349fcace26cf053862a4996a53e0
Charlie Turner <84701489+charlie-turner-314@users.noreply.github.com>
2022-10-28 14:31:08 +1000

    made manager use struct instead of int hack

--- src/config.h
+++ src/config.h
@@ -2,13 +2,13 @@
 // Name of the shared memory object
 #define SHM_NAME "PARKING"
 // Number of entrances in the parking lot
-#define NUM_ENTRANCES 1
+#define NUM_ENTRANCES 5
 // Number of exits in the parking lot
-#define NUM_EXITS 1
+#define NUM_EXITS 5
 // Number of levels in the parking lot
-#define NUM_LEVELS 1
+#define NUM_LEVELS 5
 // How many cars are allowed on each level
-#define LEVEL_CAPACITY 1
+#define LEVEL_CAPACITY 5
 // perhaps unintuitively, how much to slow time
 #define TIME_FACTOR 5
 // how much to charge customers per milisecond
